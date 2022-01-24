a89136fab7471af99587eb28662a9a5ebc4368cf
ErikVenderbush <erik.venderbush@gmail.com>
2022-01-24 13:12:51 -0500

    Initial Commit

---
+++ src/main/java/perscholas/CasestudyexampleApplication.java
@@ -0,0 +1,13 @@
+package perscholas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasestudyexampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasestudyexampleApplication.class, args);
+	}
+
+}
