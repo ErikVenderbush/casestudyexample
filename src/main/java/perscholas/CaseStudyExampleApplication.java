daaded0ae733b1fffecb96a43face6e063425b92
ErikVenderbush <erik.venderbush@gmail.com>
2022-01-24 17:31:54 -0500

    Boilerplate Spring/Tomcat Setup

---
+++ src/main/java/perscholas/CaseStudyExampleApplication.java
@@ -0,0 +1,20 @@
+package perscholas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class CaseStudyExampleApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(CaseStudyExampleApplication.class);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaseStudyExampleApplication.class, args);
+	}
+
+}
