1c0a6d7a56b70620b61679f62b5dd8822a65da6e
ErikVenderbush <erik.venderbush@gmail.com>
2022-01-25 10:46:58 -0500

    Small Form Example

--- src/main/java/perscholas/controller/IndexController.java
+++ src/main/java/perscholas/controller/IndexController.java
@@ -5,11 +5,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.ServletRequest;
+
 @Controller
 public class IndexController {
 
+    // Connects to index.jsp
     @RequestMapping(value = {"/", "/index"}, method = RequestMethod.GET)
     public ModelAndView index() throws Exception {
+        ModelAndView response = new ModelAndView();
+        response.setViewName("index");
+
+        return response;
+    }
+
+    // Connects to form action & submit
+    @RequestMapping(value = {"/indexSubmit"}, method = RequestMethod.GET)
+    public ModelAndView indexSubmit(ServletRequest request) throws Exception {
+
+        System.out.println("Username = " + request.getParameter("username"));
+
         ModelAndView response = new ModelAndView();
         response.setViewName("index");
 
--- src/main/webapp/WEB-INF/jsp/index.jsp
+++ src/main/webapp/WEB-INF/jsp/index.jsp
@@ -2,7 +2,21 @@
 
 <body>
 
-<h1>test</h1>
+    <h1>Index</h1>
+
+    <form method="get" action="/indexSubmit">
+        <label>Username</label>
+        <input type="text" name="username"><br>
+        <label>First Name</label>
+        <input type="text" name="firstName"><br>
+        <label>Dropdown
+        <select type="text" name="dropdown">
+            <option>Option 1</option>
+            <option>Option 2</option>
+            <option>Option 3</option>
+        </select><br>
+        <button type="submit">Login</button>
+    </form>
 
 </body>
 
